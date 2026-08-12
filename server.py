e58203b068ffe33076d37260d11eaf3b31f84e0e
Puput <puput8229@gmail.com>
2026-08-12 11:31:22 +0700

    Ubah port ke 5000

--- server.py
+++ server.py
@@ -338,4 +338,4 @@ def logout():
     return redirect(url_for('login_page'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
