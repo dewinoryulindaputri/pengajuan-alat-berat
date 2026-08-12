aec65580e7070a899b535c9778360b8a4f6b23f5
Puput <puput8229@gmail.com>
2026-08-12 11:38:55 +0700

    Gunakan port 8080 dinamis

--- server.py
+++ server.py
@@ -337,5 +337,8 @@ def logout():
     session.clear()
     return redirect(url_for('login_page'))
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
